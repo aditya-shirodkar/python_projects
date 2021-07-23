3b7a1860b42dd92125fbbddc9d3d3c147af3acab
Aditya Shirodkar <adityashirodkar2020@gmail.com>
2021-07-23 16:36:48 +0100

    First Commit

---
+++ gini_calculator.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 30 19:26:31 2020
+
+@author: a
+"""
+
+#this is the stage of a decision tree where gini is calculated
+#the gini is used further to decide the best point to split at
+
+class DecisionTree:
+    def __init__(self, data):
+        
+        def first(item): #function to return first element of list
+            return item[0]
+        
+        #key=first will allow sorting according only to the first element
+        #data.copy is done because direct assignment a = b.sort() gives NoneType object
+        self.data = data.copy()
+        self.data.sort(key = first)
+        
+ 
+        
+        #for checking if labels are identical/values are the same:
+        self.vals = [i[0] for i in self.data]
+        self.labels = [i[1] for i in self.data]
+        
+        #converts to set to capture unique values, and then back to list:
+        uniqueVals = list(set(self.vals))
+        uniqueVals.sort() #for some reason, the sorting from before is lost
+        
+        #creates vector to store potential splitting locations
+        self.splits = [0]*(len(uniqueVals)-1)
+        
+        #calculates all possible split values
+        for i in range(len(uniqueVals)-1):
+            self.splits[i] = (uniqueVals[i]+uniqueVals[i+1])/2
+        
+    
+    def _findBestSplit(self):
+        gini = 2.0 #to output gini of best split; default is impossible value
+        split = float("inf") #best split
+        left = [[]]
+        right = [[]] 
+        
+        if self.labels.count(self.labels[0]) == len(self.labels):
+            return False #returns false if labels are identical
+        
+        if self.vals.count(self.vals[0]) == len(self.vals):
+            return False #returns false if all values are identical
+        
+        for i in range(len(self.splits)): #iterates over all possible splits
+            
+            v = [[], []] #stores split data as list of list of lists
+            
+            partGini = [1, 1] #base, unreduced gini of the two split parts
+            giniTemp = 0 #temporary gini storage
+
+            for j in range(len(self.data)):
+                
+                #to create the two split parts
+                if self.data[j][0] < self.splits[i]:
+                    v[0].append(self.data[j])
+                else:
+                    v[1].append(self.data[j])
+            
+            for j in range(2):
+                #calculator of gini for both split parts
+                calcList0 = [1 for k in v[j] if k[1] == 0] #counter for label 0
+                calcList1 = [1 for k in v[j] if k[1] == 1] #counter for label 1
+                #stores gini for part:
+                partGini[j] -= (sum(calcList0)/len(v[j]))**2 + (sum(calcList1)/len(v[j]))**2
+                
+            #combining ginis of both parts:
+            giniTemp = (len(v[0])*partGini[0])/len(self.data) + (len(v[1])*partGini[1])/len(self.data)
+            
+            if (giniTemp < gini): #checking for lowest gini value so far
+                gini = giniTemp
+                split = self.splits[i] #identifies best split so far
+                #assigning best splits so far to left and right:
+                left = v[0]
+                right = v[1]
+            
+        return (gini, split, left, right)
+
+dataset = [[5,0],[0,1],[5,0],[10,1],[1,0], [3,0], [-1,0], [4.5, 0], [12,0],[40,1],[50,0],[50,1]]
+dt = DecisionTree(dataset)
+print(dt._findBestSplit())
+
+
+        
+                
---
+++ priority_queue/priority_queue.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 23 20:54:00 2020
+
+@author: a
+"""
+
+#queue where items are stored with a priority value which impacts item removeal
+
+class PriorityNode: #creates nodes with additional priority parameter
+    def __init__(self, content = None, priority = float("inf"), next = None):
+        self.content = content
+        self.priority = priority
+        self.next = next
+
+class PriorityQueue:
+    def __init__(self):
+        self.length = 0
+        self.head = None
+    
+    def insert(self, content, priority):
+        temp = PriorityNode(content, priority) #creates temporary node
+        
+        if not self.head: #if head is empty
+            self.head = temp #attaches new value to head
+        else:
+            if temp.priority < self.head.priority: #this is done to preserve head index
+                temp2 = self.head #preserving the current linked list
+                self.head = temp #adding new high priority value
+                self.head.next = temp2 #this operation shifts the entire list by one step
+                
+            else:
+                iterator = self.head #fresh pointer instead of using head
+                while iterator.next: #if the next node is not None
+                    if temp.priority >= iterator.next.priority: #want to see if next...
+                        iterator = iterator.next #...element has better (lower) priority
+                    else:
+                        break #stops iterating at point where new value is inserted
+                
+                if iterator.next: #similar to the head code, places the value after...
+                    temp2 = iterator.next #...end of same priority and before worse...
+                    iterator.next = temp #...(higher) priority, shifting the worse...
+                    iterator.next.next = temp2 #...elements to the right.
+                else: #if there is no worse priority, simply adds value to the end
+                    iterator.next = temp
+                
+        self.length += 1 #increments length of linked list
+        
+        return True #returns if insert was possible
+        
+    def remove(self): #same as regular queue
+        if (self.length > 0):
+            temp = self.head.content
+            self.head = self.head.next
+            self.length = self.length - 1
+            return temp
+        else:
+            return False
+    
+    def clear(self):
+        self.length = 0
+        self.head = None
+        
+    def is_empty(self):
+        return (self.length == 0)
+
+
+
+queue = PriorityQueue()
+
+queue.insert(5,1)
+queue.insert(7,3)
+queue.insert(8,2)
+queue.insert(12,3)
+queue.insert(15,4)
+queue.insert(20,2)
+queue.insert(1,1)
+
+while (not queue.is_empty()):
+    print(queue.remove())
